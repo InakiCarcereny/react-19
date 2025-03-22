dc1eecfde179ef09ae43c38a665abb1f6099a39a
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-22 14:11:18 -0300

    Use case metadata

---
+++ src/components/Seo1.tsx
@@ -0,0 +1,8 @@
+export function Seo1() {
+  return (
+    <>
+      <title>Seo1</title>
+      <meta name="description" content="Seo1" />
+    </>
+  );
+}
---
+++ src/components/Seo2.tsx
@@ -0,0 +1,8 @@
+export function Seo2() {
+  return (
+    <>
+      <title>Seo2</title>
+      <meta name="description" content="Seo2" />
+    </>
+  );
+}
